76a281854a051b05c452e26336cdad1b13eb7464
Becky Zahid <buzahid@microsoft.com>
2024-02-22 15:55:39 -0800

    Separated solution and starter code

---
+++ AITravelAgent/Starter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.Plugins.Core;
+#pragma warning disable SKEXP0050 
+#pragma warning disable SKEXP0060
+
+string yourDeploymentName = "";
+string yourEndpoint = "";
+string yourKey = "";
+
+var builder = Kernel.CreateBuilder();
+builder.Services.AddAzureOpenAIChatCompletion(
+    yourDeploymentName,
+    yourEndpoint,
+    yourKey,
+    "gpt-35-turbo-16k");
+var kernel = builder.Build();
+
+// Note: ChatHistory isn't working correctly as of SemanticKernel v 1.4.0
+StringBuilder chatHistory = new();
+
+kernel.ImportPluginFromType<ConversationSummaryPlugin>();
+var prompts = kernel.ImportPluginFromPromptDirectory("Prompts");
+
